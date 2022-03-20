dab3f409f99f455f899be240ec6a4a6b0dd57dc4
Nippun Kumaar <nippun05@gmail.com>
2022-03-20 11:59:22 +0530

    added obstacle_avoidance controller

---
+++ Four Wheel Robot/controllers/Obstancle_Avoidance/build/release/Obstancle_Avoidance.d
@@ -0,0 +1,6 @@
+build/release/Obstancle_Avoidance.o: Obstancle_Avoidance.c \
+  /Applications/Webots.app/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/include/controller/c/webots/types.h \
+  /Applications/Webots.app/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/include/controller/c/webots/distance_sensor.h
